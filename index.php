62c1d20dbe5cd029f4a20f54834a60b4f785043f
Soupart Mathieu <soupart.mathieu@gmail.com>
2016-03-13 13:43:30 +0100

    Added bootstrap and a navbar

--- index.php
+++ index.php
@@ -76,12 +76,23 @@ else if (isset($_POST['led3Off']))
 }
 ?>
 <html>
+<link href="css/bootstrap.min.css" rel="stylesheet">
 <body>
+<header>
+    <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse">
+                <span class="sr-only">Toggle navigation</span>
+            </button>
+        </div>
+        <a class="navbar-brand" href="#">Cocktail Machine</a>
+    </nav>
+</header>
 <form method="post">
     <p>
-        <button name="led1On" class="On">Led 1 On</button>
-        <button name="led2On" class="On">Led 2 On</button>
-        <button name="led3On" class="On">Led 3 On</button>
+        <button name="led1On" class="btn btn-default">Sequence 1</button>
+        <button name="led2On" class="btn btn-default">Sequence 2</button>
+        <button name="led3On" class="btn btn-default">Sequence 3</button>
     </p>
     <p>
         <button name="led1Off" class="Off">Led 1 Off</button>
